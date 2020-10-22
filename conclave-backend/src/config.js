86076ef147b6054ab753af77556655d6b6e7edca
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2020-10-23 04:52:33 +0530

    Updating config.js to accommodate the DB connection env variables

--- conclave-backend/src/config.js
+++ conclave-backend/src/config.js
@@ -1,3 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 export const welcomeText = process.env.WELCOME_TEXT;
+
+export const dbDialect = process.env.DATABASE_DIALECT;
+export const dbHost = process.env.DATABASE_HOST;
+export const dbName = process.env.DATABASE_NAME;
+export const dbUser = process.env.DATABASE_USER;
+export const dbPassword = process.env.DATABASE_PASSWORD;
