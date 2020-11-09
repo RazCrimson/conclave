58c71536ee750f573a6e721da6c47c6f7255e480
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2020-11-09 22:00:06 +0530

    Dropping server-side sessions

--- conclave-backend/src/app.js
+++ conclave-backend/src/app.js
@@ -1,11 +1,9 @@
 import logger from 'morgan';
 import express from 'express';
-import session from 'express-session';
 
 import passport from 'passport';
 
 import './connections/databaseInitializer';
-import sessionConfig from './connections/sessionInitializer';
 
 import indexRouter from './routes/index';
 
@@ -15,10 +13,9 @@ app.use(logger('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session(sessionConfig));
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/api', indexRouter);
+app.use('/auth', authRouter);
 
 export default app;
