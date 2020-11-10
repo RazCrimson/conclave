0bd5ef164fba90391da50588392ffeffa26ab4d3
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2020-11-11 05:13:00 +0530

    Updated authControllers and routers to match the new strategies

--- conclave-backend/src/controllers/authController.js
+++ conclave-backend/src/controllers/authController.js
@@ -1,14 +1,46 @@
-// eslint-disable-next-line no-unused-vars
-export const signIn = async (req, res, next) => {};
+import User from '../models/userModel';
 
 // eslint-disable-next-line no-unused-vars
-export const signUp = async (req, res, next) => {};
+export const signIn = async (req, res) => {
+  const accessToken = await User.createAccessToken(req.user);
+  const refreshToken = await User.createRefreshToken(req.user);
+  console.log('Tokens : ', accessToken, refreshToken);
+
+  res.cookie('jwt', accessToken, { maxAge: 900000, httpOnly: true });
+  res.cookie('ref', refreshToken, { maxAge: 9000000, httpOnly: true });
+
+  return res.status(200).json({ data: { message: 'Logged In' } });
+};
 
 // eslint-disable-next-line no-unused-vars
-export const signOut = async (req, res, next) => {};
+export const signUp = async (req, res) => {
+  const { username, password, email } = req.body;
+  // TODO: Sanitize inputs find by regex.
+
+  const usernameFound = await User.findOne({ where: { username } });
+  if (usernameFound) return res.status(406).json({ data: { detail: 'Username already taken.', status: -11 } });
+
+  const emailFound = await User.findOne({ where: { email } });
+  if (emailFound) return res.status(406).json({ data: { detail: 'Email already in use.', status: -12 } });
+
+  const user = await User.create({ username, password, email });
+  console.debug('Created a new account: ', user.userID);
+
+  const accessToken = await User.createAccessToken(user);
+  const refreshToken = await User.createRefreshToken(user);
+
+  res.cookie('refreshToken', refreshToken, { maxAge: 900000, httpOnly: true });
+
+  return res.status(201).json({ data: { message: 'Account Created', accessToken, refreshToken } });
+};
 
 // eslint-disable-next-line no-unused-vars
-export const loginStatus = async (req, res, next) => {};
+export const signOut = async (req, res) => {};
 
 // eslint-disable-next-line no-unused-vars
-export const refreshSession = async (req, res, next) => {};
+export const loginStatus = async (req, res) => {
+  res.json({ username: req.user.username });
+};
+
+// eslint-disable-next-line no-unused-vars
+export const generateAuthToken = async (req, res) => {};
--- conclave-backend/src/routes/auth.js
+++ conclave-backend/src/routes/auth.js
@@ -1,13 +1,14 @@
 import express from 'express';
 
-import { signUp, signIn, signOut, loginStatus, refreshSession } from '../controllers/authController';
+import { jwtAuth, localSignInAuth } from '../middlewares/passport/passportConfig';
+import { signUp, signIn, signOut, loginStatus, generateAuthToken } from '../controllers/authController';
 
 const authRouter = express.Router();
 
 authRouter.post('/signUp', signUp);
-authRouter.post('/signIn', signIn);
+authRouter.post('/signIn', localSignInAuth, signIn);
 authRouter.get('/signOut', signOut);
-authRouter.get('/status', loginStatus);
-authRouter.post('/refreshSession', refreshSession);
+authRouter.get('/status', jwtAuth, loginStatus);
+authRouter.post('/refreshSession', generateAuthToken);
 
 export default authRouter;
